ac3b01d961c999fe78f52d7a89a49d69893b7453
Vinh Bui <vinh.bui@ziften.com>
2016-07-21 13:33:59 -0500

    Change var names so can copy & paste jenkins server info

--- prep_server_for_dataquality/prep.sh
+++ prep_server_for_dataquality/prep.sh
@@ -1,37 +1,37 @@
-ZIFTEN_SERVER=ec2-54-197-200-41.compute-1.amazonaws.com
-VERTICA_SERVER=ec2-54-174-97-21.compute-1.amazonaws.com
+ZIFTEN_CNAME=ec2-52-91-172-20.compute-1.amazonaws.com
+VERTICA_CNAME=ec2-54-164-108-31.compute-1.amazonaws.com
 SSL_CERT_PATH=~/.ssh/qa.pem
 
 # install zip on ziften server
-ssh -i $SSL_CERT_PATH root@$ZIFTEN_SERVER 'yes | yum install zip'
+ssh -i $SSL_CERT_PATH root@$ZIFTEN_CNAME 'yes | yum install zip'
 
 # zip resources dir on ziften server
-ssh -i $SSL_CERT_PATH root@$ZIFTEN_SERVER 'zip -r resources.zip /opt/ziften/resources'
+ssh -i $SSL_CERT_PATH root@$ZIFTEN_CNAME 'zip -r resources.zip /opt/ziften/resources'
 
 # download resources.zip
-scp -i $SSL_CERT_PATH root@$ZIFTEN_SERVER:/root/resources.zip .
+scp -i $SSL_CERT_PATH root@$ZIFTEN_CNAME:/root/resources.zip .
 
 # upload resources.zip to Vertica server
-scp -i $SSL_CERT_PATH resources.zip root@$VERTICA_SERVER:/
+scp -i $SSL_CERT_PATH resources.zip root@$VERTICA_CNAME:/
 
 # install unzip
-ssh -i $SSL_CERT_PATH root@$VERTICA_SERVER 'yes | yum install unzip'
+ssh -i $SSL_CERT_PATH root@$VERTICA_CNAME 'yes | yum install unzip'
 
 # unzip resources.zip on Vertica server
-ssh -i $SSL_CERT_PATH root@$VERTICA_SERVER 'yes | unzip /resources.zip -d /'
+ssh -i $SSL_CERT_PATH root@$VERTICA_CNAME 'yes | unzip /resources.zip -d /'
 
 # create /opt/ziften/tmp
-ssh -i $SSL_CERT_PATH root@$VERTICA_SERVER 'mkdir -p /opt/ziften/tmp'
+ssh -i $SSL_CERT_PATH root@$VERTICA_CNAME 'mkdir -p /opt/ziften/tmp'
 
 # change owner and group of tmp folder
-ssh -i $SSL_CERT_PATH root@$VERTICA_SERVER 'chown ziften /opt/ziften/tmp'
-ssh -i $SSL_CERT_PATH root@$VERTICA_SERVER 'chown :ziften /opt/ziften/tmp'
+ssh -i $SSL_CERT_PATH root@$VERTICA_CNAME 'chown ziften /opt/ziften/tmp'
+ssh -i $SSL_CERT_PATH root@$VERTICA_CNAME 'chown :ziften /opt/ziften/tmp'
 
 # download zlabmq.settings file from ziften server
-scp -i $SSL_CERT_PATH root@$ZIFTEN_SERVER:/opt/ziften/zlabmq.settings .
+scp -i $SSL_CERT_PATH root@$ZIFTEN_CNAME:/opt/ziften/zlabmq.settings .
 
 # upload zlabmq.settings file from ziften server
-scp -i $SSL_CERT_PATH zlabmq.settings root@$VERTICA_SERVER:/opt/ziften/
+scp -i $SSL_CERT_PATH zlabmq.settings root@$VERTICA_CNAME:/opt/ziften/
 
 rm -f resources.zip
 rm -f zlabmq.settings
